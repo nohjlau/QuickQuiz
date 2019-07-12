3d7e422cb616f87ce72e0d54c0c499cddd244c1f
John Lau <nohjlau@gmail.com>
2019-07-12 10:43:50 -0700

    Added HUD display.

--- game.js
+++ game.js
@@ -1,6 +1,7 @@
 const question = document.getElementById('question');
 const choices = Array.from(document.getElementsByClassName("choice-text"));
-
+const questionCounterText = document.getElementById('questionCounter');
+const scoreText = document.getElementById('score');
 let currentQuestion = {};
 let acceptingAnswers = false;
 let score = 0;
@@ -53,6 +54,7 @@ let questions = [
           return window.location.assign('/end.html');
       }
       questionCounter++;
+      questionCounterText.innerText = questionCounter + "/" + MAX_QUESTIONS;
       const questionIndex = Math.floor(Math.random() * availableQuestions.length);
       currentQuestion = availableQuestions[questionIndex];
       question.innerText = currentQuestion.question;
@@ -78,10 +80,17 @@ choices.forEach(choice => {
         
         const classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect';
         
+        if(classToApply === 'correct')
+          incrementScore(CORRECT_BONUS);
         selectedChoice.parentElement.classList.add(classToApply);
         setTimeout( () => { selectedChoice.parentElement.classList.remove(classToApply);
           getNewQuestion();}, 1000);
        
    });
 });
+
+incrementScore = num => {
+  score += num;
+  scoreText.innerText = score;
+}
 startGame();
