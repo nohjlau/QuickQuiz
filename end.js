6b03ce7a1f08934ddf54d87b2578b9ecc09c652c
John Lau <nohjlau@gmail.com>
2019-07-14 20:12:10 -0700

    Added highscores logic.

--- end.js
+++ end.js
@@ -1,17 +1,30 @@
 const username = document.getElementById('username');
 const saveScoreBtn = document.getElementById('save-score-button');
-
-const mostRecentScore = localStorage.getItem('mostRecentScore');
 const finalScore = document.getElementById('final-score');
+const mostRecentScore = localStorage.getItem('mostRecentScore');
 
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 finalScore.innerText = mostRecentScore;
+
+const MAX_HIGH_SCORES = 5;
 
 username.addEventListener('keyup', () => {
     saveScoreBtn.disabled = !username.value;
 });
 
 saveHighScore = (e) => {
-    console.log("Clicked the save button!");
     e.preventDefault();
 
-}
+    const score = {
+        score: mostRecentScore,
+        name: username.value
+    };
+    
+    highScores.push(score);
+    highScores.sort((a,b) => b.score - a.score)
+    highScores.splice(5);
+
+    localStorage.setItem('highScores', JSON.stringify(highScores));
+    window.location.assign("/");
+
+};
